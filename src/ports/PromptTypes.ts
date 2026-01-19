aaf518c7d24e71c43f16c0f00ddfbc342867fc46
Leonardo Pires <me@leonardopires.net>
2026-01-19 10:50:03 -0300

    Add folder prefill options for new Booker files

---
+++ src/ports/PromptTypes.ts
@@ -0,0 +1,8 @@
+/**
+ * Result returned from the filename prompt modal.
+ */
+export type FilenamePromptResult = {
+  filename: string;
+  prefillFromFolder: boolean;
+  includeSubfolders: boolean;
+};
